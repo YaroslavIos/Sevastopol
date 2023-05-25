6e609c86b63e9a6007fe5f7eb89390fa9b1f1139
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-05-25 22:54:59 +0300

    Initial Commit

---
+++ Sevastopol/SevastopolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevastopolApp.swift
+//  Sevastopol
+//
+//  Created by Ярослав Любиченко on 25.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct SevastopolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
