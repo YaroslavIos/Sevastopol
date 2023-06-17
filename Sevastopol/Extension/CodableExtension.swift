3addddd6200f6ffd033232a36a893b88dfb0042c
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-06-17 23:42:48 +0300

    Extension for decoding json file.

---
+++ Sevastopol/Extension/CodableExtension.swift
@@ -0,0 +1,28 @@
+//
+//  CodableExtension.swift
+//  Sevastopol
+//
+//  Created by Ярослав Любиченко on 25.5.2023.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to locate \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
